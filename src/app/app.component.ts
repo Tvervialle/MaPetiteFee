398fb6fa0ca3efd6239a86a53f8dbe9d7497d74c
Thomas VERVIALLE <ThomasV@acelys.local>
2019-04-18 16:47:38 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MaPetiteFee';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyCmuDxYsR3tFAyMyoB9cSPB6KIi4vLqkH8",
+      authDomain: "mapetitefee-a498f.firebaseapp.com",
+      databaseURL: "https://mapetitefee-a498f.firebaseio.com",
+      projectId: "mapetitefee-a498f",
+      storageBucket: "mapetitefee-a498f.appspot.com",
+      messagingSenderId: "1080544141771"
+    };
+    firebase.initializeApp(config);
+  }
 }
